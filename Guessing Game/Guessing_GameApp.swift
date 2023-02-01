83a778c78f3ed9488ed7812d3faccb2e7412a483
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-02-01 10:18:57 -0500

    Initial Commit

---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Graeme Armstrong on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
